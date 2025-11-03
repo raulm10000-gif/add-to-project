9583ad46940f88a9ee2ae9b6a746a21119958575
raulm10000-gif <raulm.10000@gmail.com>
2025-11-03 03:58:42 -0600

    Remove addToProject execution from main.ts
    Null

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,1 @@
-import * as core from '@actions/core'
-import {addToProject} from './add-to-project'
-
-addToProject()
-  .catch(err => {
-    core.setFailed(err.message)
-    process.exit(1)
-  })
-  .then(() => {
-    process.exit(0)
-  })
+src/add-to-project.ts
